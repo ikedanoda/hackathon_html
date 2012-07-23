b419676ec53c142c01b518f76f19a03e9ffc4ec9
IKEDA YOSUKE <ikeda@ale-cole.com>
2012-07-23 14:42:27 +0900

    テストコミット
    テストコミット
    
    Signed-off-by: IKEDA YOSUKE <ikeda@ale-cole.com>

---
+++ testfile.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="ja" xmlns:og="http://ogp.me/ns#" xmlns:mixi="http://mixi-platform.com/ns#">
+<head>
+<meta charset="utf-8">
+<title>TOP</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta property="og:title" content="ページタイトル">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="サイト名">
+<meta property="og:image" content="http://blog.sinap.jp/res/images/common/OGP.png">
+<meta property="fb:app_id" content="120475414763656">
+<meta property="og:locale" content="ja_JP">
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/file_read.php'; ?>
+</head>
+<body>
+<div id="wrapper">
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php'; ?>
+
+<div id="contentsWrapper">
+<div id="topPage">
+<ul id="slideBtn">
+<li id="btnPrev"><img src="/assets/images/common/btn_slide_prev_01.png" alt="左へ" width="13" height="223"></li>
+<li id="btnNext"><img src="/assets/images/common/btn_slide_next_01.png" alt="右へ" width="13" height="223"></li>
+</ul>
+
+<div id="slideContents" class="east">
+<ul class="shelfList01 row01">
+<li id="e-1-1"><img src="/assets/images/list/empty_h117.jpg" alt="" width="203" height="117"></li>
+<li id="e-1-2"><img src="/assets/images/list/empty_h117.jpg" alt="" width="203" height="117"></li>
+<li id="e-1-3"><img src="/assets/images/list/empty_h117.jpg" alt="" width="203" height="117"></li>
+<li id="e-1-4"><img src="/assets/images/list/empty_h117.jpg" alt="" width="203" height="117"></li>
+<li id="e-1-5" class="detail"><img src="/assets/images/list/east/e-1-5.jpg" alt="たもっさんの本棚" width="203" height="117"></li>
+<li id="e-1-6" class="detail"><img src="/assets/images/list/east/e-1-6.jpg" alt="たもっさんの本棚" width="203" height="117"></li>
+<li id="e-1-7" class="detail"><img src="/assets/images/list/east/e-1-7.jpg" alt="たもっさんの本棚" width="203" height="117"></li>
+<li id="e-1-8" class="detail"><img src="/assets/images/list/east/e-1-8.jpg" alt="たもっさんの本棚" width="203" height="117"></li>
+</ul>
+
+<ul class="shelfList01 row02">
+<li id="e-2-1"></li>
+<li id="e-2-2"><img src="/assets/images/list/empty_h148.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-2-3"><img src="/assets/images/list/empty_h148.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-2-4"><img src="/assets/images/list/empty_h148.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-2-5" class="detail"><img src="/assets/images/list/east/e-2-5.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-2-6" class="detail"><img src="/assets/images/list/east/e-2-6.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-2-7" class="detail"><img src="/assets/images/list/east/e-2-7.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-2-8" class="detail"><img src="/assets/images/list/east/e-2-8.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+</ul>
+
+<ul class="shelfList01 row03">
+<li id="e-3-1" class="detail"><img src="/assets/images/list/east/e-3-1.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-3-2" class="detail"><img src="/assets/images/list/east/e-3-2.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-3-3" class="detail"><img src="/assets/images/list/east/e-3-3.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-3-4" class="detail"><img src="/assets/images/list/east/e-3-4.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-3-5" class="detail"><img src="/assets/images/list/east/e-3-5.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-3-6" class="detail"><img src="/assets/images/list/east/e-3-6.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-3-7" class="detail"><img src="/assets/images/list/east/e-3-7.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-3-8" class="detail"><img src="/assets/images/list/east/e-3-8.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+</ul>
+
+<ul class="shelfList01 row04">
+<li id="e-4-2" class="detail"><img src="/assets/images/list/east/e-4-2.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-4-3" class="detail"><img src="/assets/images/list/east/e-4-3.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-4-4" class="detail"><img src="/assets/images/list/east/e-4-4.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-4-5" class="detail"><img src="/assets/images/list/east/e-4-5.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-4-6" class="detail"><img src="/assets/images/list/east/e-4-6.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-4-7" class="detail"><img src="/assets/images/list/east/e-4-7.jpg" alt="たもっさんの本棚" width="203" height="148"></li>
+<li id="e-4-8"><img src="/assets/images/list/east/e-4-8.jpg" alt="wifi" width="203" height="148"></li>
+</ul>
+
+<ul class="shelfList01 row05">
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+<li id="e-5-1" class="detail"><img src="/assets/images/list/east/e-5-1.jpg" alt="たもっさんの本棚" width="203" height="156"></li>
+</ul>
+</div><!-- /#slideContents.wrapInner -->
+
+</div><!-- /#topPage.wrapInner -->
+</div><!-- /#contentsWrapper -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php'; ?>
+<script type="text/javascript" src="/assets/javascripts/top.js" charset="utf-8"></script>
+<script type="text/javascript" src="/assets/javascripts/chikara.js" charset="utf-8"></script>
+</div><!-- /#wrapper -->
+</body>
+</html>
